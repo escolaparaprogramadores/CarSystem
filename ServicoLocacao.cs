2e1712f1139a095c7043df3be9b149ffd896e70c
Mark Roger <markroger@Marks-iMac.local>
2022-07-01 23:20:56 -0300

    adicionando a classe ServocoLocacao

---
+++ ServicoLocacao.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CarSystem
+{
+    public class ServicoLocacao
+    {
+        public DateTime DataLocacao {get; set;} 
+        public decimal ValorDiaria {get; set;} 
+        public int QuantidadeDiaLocacao {get; set;} 
+        public decimal ValorTotalLocacao {get; set;} 
+    }
+}
+
