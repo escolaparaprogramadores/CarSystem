18d0d92f4682075964d75d43f8517da8060d8896
Mark Roger <markroger@Marks-iMac.local>
2022-07-06 22:56:16 -0300

    Aula 05 - Criação da classe Endereço

--- Cliente.cs
+++ Cliente.cs
@@ -4,17 +4,13 @@ namespace CarSystem
 {
     public class Cliente
     {
-        //Tipos de dados      
+        //Tipos de dados primitivo     
        int QuantidadeDeFilhos {get; set;}
        string Nome {get; set;}
        string Cpf {get; set;}
        DateTime DataNascimento {get; set;}
-       string Cidade {get; set;}
-       string Bairro {get; set;}
-       string Logradouro {get; set;}
-       string Numero {get; set;}
-       string Cep {get; set;}
        bool ECasado {get; set;}
        Decimal Renda {get; set;} //Double ou Float
+       Endereco Endereco {get; set;} // Tipo de objeto
     }
 }
