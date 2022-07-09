228839944e9f773281bd6313ac6dba48e9109759
Mark Roger <markroger@Marks-iMac.local>
2022-07-08 23:38:55 -0300

    Aula 07 - Criando métodos

--- Program.cs
+++ Program.cs
@@ -8,19 +8,7 @@ namespace CarSystem
         static void Main(string[] args)
         {
             //Objeto 1
-           new ServicoLocacao1
-           (
-            200,
-            30
-           );
-
-           //Objeto 2
-           new ServicoLocacao1
-           (
-            200,
-            2
-           );
-          
+           new ServicoLocacao1(vlrDiaria: 200, qtdDiaLocacao: 30);  
 
             Console.WriteLine(25*5/2+298);
         }
